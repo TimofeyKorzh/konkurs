9f6d8b2cd638ebd6e26ca7525d1a081ad1f52bfd
MyLittleCoin <mylittlecoin1@gmail.com>
2021-03-24 15:28:24 +0300

    updated links

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-const ENDPOINT_URL = "http://coffeemachine.mc4ep.org:38134/generate";
+const ENDPOINT_URL = "https://bot.sweetie.tech:38134/generate";
 
 const useAsyncEndpoint = fn => {
     const [res, setRes] = React.useState({ data: null, complete: false, pending: false, error: false });
